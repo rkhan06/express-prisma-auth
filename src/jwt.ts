9d02315411bb1705d00ba78fc6edcd93c3da5bc6
Rameen <rameen.khan@scratchpay.com>
2024-07-17 17:05:34 +0500

    feat: added regenerate token method

--- src/jwt.ts
+++ src/jwt.ts
@@ -1,5 +1,23 @@
 import jwt from "jsonwebtoken";
 
-export const generateToken = (userId: number, jwtSecret: string) => {
+export const generateToken = (
+  userId: number | string,
+  jwtSecret: string,
+  refreshSecret: string,
+) => {
+  const accessToken = jwt.sign({ userId }, jwtSecret, { expiresIn: "1h" });
+  const refreshToken = jwt.sign({ userId }, refreshSecret, { expiresIn: "7d" });
+
+  return { accessToken, refreshToken };
+};
+
+export const verifyRefreshToken = (token: string, refreshSecret: string) => {
+  return jwt.verify(token, refreshSecret);
+};
+
+export const regenerateAccessToken = (
+  userId: number | string,
+  jwtSecret: string,
+) => {
   return jwt.sign({ userId }, jwtSecret, { expiresIn: "1h" });
 };
